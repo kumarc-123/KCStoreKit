c4a87ae71979f9b3402e61bf0271247b6f3dd571
Kumar C <kumarchikkanna1@gmail.com>
2016-04-19 17:57:49 +0530

    Initial Commit

---
+++ KCStoreKit/KCStoreKit.h
@@ -0,0 +1,19 @@
+//
+//  KCStoreKit.h
+//  KCStoreKit
+//
+//  Created by Kumar C on 4/19/16.
+//  Copyright © 2016 Kumar C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCStoreKit.
+FOUNDATION_EXPORT double KCStoreKitVersionNumber;
+
+//! Project version string for KCStoreKit.
+FOUNDATION_EXPORT const unsigned char KCStoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCStoreKit/PublicHeader.h>
+
+
